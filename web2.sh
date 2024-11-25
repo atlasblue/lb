19af6f0d5e79bc32b53d8be36b73eae17287d357
atlasblue <92083755+atlasblue@users.noreply.github.com>
2024-11-25 11:52:46 +0100

    Update web2.sh

--- web2.sh
+++ web2.sh
@@ -1,5 +1,4 @@
 sudo dnf install nginx wget -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
-serverRoot="/usr/share/nginx/html"
-wget https://raw.githubusercontent.com/atlasblue/lb/master/index-web2.html -P $serverRoot
+wget https://raw.githubusercontent.com/atlasblue/lb/master/index-web2.html -O /usr/share/nginx/html/index.html
