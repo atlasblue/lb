a7c863f8a87b8d1a3554a6d7f0c365dfa6a4e534
atlasblue <92083755+atlasblue@users.noreply.github.com>
2024-12-31 21:11:19 +0100

    Update web1.sh

--- web1.sh
+++ web1.sh
@@ -1,4 +1,8 @@
 sudo dnf install nginx wget -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
-wget https://raw.githubusercontent.com/atlasblue/lb/master/index-web1.html -O /usr/share/nginx/html/index.html
+sudo rm /usr/share/nginx/html/index.html
+cat <<EOF >>/usr/share/nginx/html/index.html
+<h1>This is Webserver 1</h1>
+EOF
+
